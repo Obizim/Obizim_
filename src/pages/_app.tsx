d55a493dcc36346511595b03e780314d6c73b5e1
Zimuzo Obi <obizimuzo@gmail.com>
2023-07-20 00:53:23 +0100

    edit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="rgba(0, 0, 0, 1)"></meta>
+        <meta content="Software Developer" name="description"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:site_name" content="Zimuzo Obi"></meta>
+        <meta property="og:description" content="Software Developer."/>
+        <meta property="og:url" content="https://catcave.vercel.app/" />
+        <meta property="og:image" content="" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Zimuzo Obi - SoftwareDeveloper" />
+        <meta name="twitter:title" content="Zimuzo Obi - Software Developer"></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta name="twitter:description" content=""></meta>
+        <meta name="twitter:image" content=""></meta>
+        <title>Zimuzo Obi - Software Developer</title>
+      </Head>
+  <Component {...pageProps} />
+  </>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
           Software frontend developer based in UK, creating great & accesible user
           interfaces.
         </p>
-        <p className="text-gray-300 w-full max-w-2xl"> I'm Zimuzo Obi, a goal-oriented developer, bringing strong dedication to collaboration and a solutions-oriented approach to problem-solving.
+        <p className="w-full max-w-2xl"> I'm Zimuzo Obi, a goal-oriented developer, bringing strong dedication to collaboration and a solutions-oriented approach to problem-solving.
         Higly attentive to details with strong work ethic as well as adept at implementing new technologies and techniques 
         </p>
-        <div className="text-gray-300 w-full max-w-2xl">I have experience with the following technologies and languages: 
+        <div className="w-full max-w-2xl">I have experience with the following technologies and languages: 
           <ul className='list-inside list-disc py-4'>
             <li>Languages: HTML, CSS (SCSS), Javascript, Typescript</li>
             <li>Frameworks/Tools: React.js, Next.js, Redux, Tailwind CSS, Bootstrap,Node, Express.js</li>
@@ -25,7 +25,7 @@ export default function Home() {
 
       <section className="mt-10">
         <div className="relative space-y-2">
-          <p className="text-2xl text-gray-300 w-max capitalize">Featured Projects</p>
+          <p className="text-2xl w-max capitalize">Featured Projects</p>
           <p>Here are a few examples of intriguing projects I have created:</p>
         </div>
 
@@ -33,7 +33,7 @@ export default function Home() {
           {works.map(data => (
             <a href={data.url} target="blank" className="hover:bg-burger-bg relative border-b border-header_bg flex justify-between items-end py-4 px-2 cursor-pointer">
             <div className="flex items-end space-x-2">
-              <p className="self-start text-gray-300">0{data.id}.</p>
+              <p className="self-start">0{data.id}.</p>
               <div className="space-y-2">
               <h2>{data.title}</h2>
               <p>{data.description}</p>
@@ -47,7 +47,8 @@ export default function Home() {
       <footer className="my-10 relative space-y-6">
           <p>I am actively seeking new opportunities and welcome connections or collaborations on projects. 
             You can reach out to me on <a className='underline' href='https://twiiter.com/zimway_' target='_blank'>Twitter</a> or on <a className='underline' href='https://www.linkedin.com/in/obizim/' target='_blank'>Linkedin</a></p>
-            <p>You can <a className='underline' href="mailto:obizimuzo@gmail.com">message</a> me as well. Don't hesistate to reach out to me</p>
+            <p>You can <a className='underline' href="mailto:obizimuzo@gmail.com">message</a> me as well. Don't hesistate to reach out to me.</p>
+            <p className="self-end"> &copy; Zimuzo Obi {new Date().getFullYear()}.</p>
       </footer>
     </main>
   )
